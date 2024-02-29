b3a1f47cf8c61d8942fda8da5bd045a405a74b3f
unknown <njema3@gmail.com>
2024-02-29 20:05:06 +0300

    Add existing file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-document.querySelector("")
+const paymentMethods = [
+    {
+        img: "./images/bank.jpeg",
+        title: "Bank"
+    }, 
+    {
+        img: "./images/master.png",
+        title: "MasterCard"
+    },
+    {
+        img: "./images/visa.jpeg",
+        title: "Visa"
+    }, 
+    {
+        img: "./images/paypal.png",
+        title: "Paypal"
+    },
+    {
+        img: "./images/skrill.png",
+        title: "Skrill"
+    }
+]
