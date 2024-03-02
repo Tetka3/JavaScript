3f89053dab9f2259d3a784454f2a725097e09863
unknown <njema3@gmail.com>
2024-03-02 18:37:23 +0300

    Add existing file

--- script.js
+++ script.js
@@ -1,22 +1,110 @@
-const paymentMethods = [
+const products = [
     {
-        img: "./images/bank.jpeg",
-        title: "Bank"
+        id: 1,
+        img: "./images/shoe2.jpeg",
+        title: "Rubber Shoes"
     }, 
     {
-        img: "./images/master.png",
-        title: "MasterCard"
+        id: 2,
+        img: "./images/shoe3.jpeg",
+        title: "Rubber Shoes"
     },
     {
-        img: "./images/visa.jpeg",
-        title: "Visa"
+        id: 3,
+        img: "./images/shoe1.jpeg",
+        title: "Rubber Shoes"
     }, 
     {
-        img: "./images/paypal.png",
-        title: "Paypal"
+        id: 4,
+        img: "./images/bag1.jpeg",
+        title: "My Handbag"
     },
     {
-        img: "./images/skrill.png",
-        title: "Skrill"
+        id: 5,
+        img: "./images/bag2.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 6,
+        img: "./images/bag3.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 7,
+        img: "./images/bag4.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 8,
+        img: "./images/bag2.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 9,
+        img: "./images/bag1.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 10,
+        img: "./images/bag2.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 11,
+        img: "./images/bag3.jpeg",
+        title: "My Handbag"
+    },
+    {
+        id: 12,
+        img: "./images/bag4.jpeg",
+        title: "My Handbag"
     }
-]
+];
+
+let productsHTML = '';
+
+products.forEach((product) => {
+    productsHTML += `
+    <div class="product">
+        <img src="${product.img}" />
+        <p>${product.title}</p>
+        <button class="js-add-to-cart" data-product-id="${product.id}">Add To Cart</button>
+    </div>
+    `
+    
+})
+
+document.querySelector('.js-products').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            let matchingItem; 
+
+            cart.forEach((item) => {
+                if (productId === item.productId) {
+                    matchingItem = item;
+                }
+            });
+            if (matchingItem) {
+                matchingItem.quantity += 1;
+            } else {
+                cart.push({
+                    productId: productId,
+                    quantity: 1
+                });
+            }
+            
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-quantity').innerHTML = cartQuantity;
+            console.log(cartQuantity);
+            console.log(cart);
+        });
+    });
